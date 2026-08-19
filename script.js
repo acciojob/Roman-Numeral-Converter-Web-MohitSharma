1642c0b5d412e1142acdbbfa942a7f9a3dc82b31
Mohit Sharma <skmohit9955@gmail.com>
2026-08-19 21:09:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,18 +14,25 @@ function convertToRoman(num) {
         ['IV', 4],
         ['I', 1]
     ];
+	const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
 
-    let result = '';
+    let result = "";
 
-    for (let [symbol, value] of obj) {
-        while (num >= value) {
-            result += symbol;
-            num -= value;
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            result += symbols[i];
+            num -= values[i];
         }
     }
 
     return result;
+		
+	
+	
+    
 }
+
 
 console.log(convertToRoman(36));
 
